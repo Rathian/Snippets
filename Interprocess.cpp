77e91cf9cf4e4eb9fc1a22b3d66a67e7e258123a
Rathian <bastian.rioreia@gmail.com>
2019-05-05 20:45:07 +0200

    added thread safe iostream

--- Interprocess.cpp
+++ Interprocess.cpp
@@ -10,8 +10,222 @@
 #include <vector>
 #include <thread>
 #include <chrono>
+#include <mutex>
+#include <memory>
+#include <condition_variable>
+#include <algorithm>
 
 namespace bip = boost::interprocess;
+
+
+class block_buffer : public std::enable_shared_from_this<block_buffer>
+{
+public:
+	block_buffer(std::size_t size)
+		: _memory(static_cast<char*>(malloc(size)))
+		, _capacity(size)
+		, _size(0)
+		, _next(nullptr)
+		, _lock()
+	{ }
+
+	~block_buffer() {
+		free(_memory);
+	}
+
+	std::size_t size() {
+		if (_size < _capacity)
+			return _size;
+		else if (_next)
+			return _capacity + _next->size();
+		else
+			return _size;
+	}
+
+	std::shared_ptr<block_buffer> read(char* buffer, std::size_t size, std::size_t& offset) {
+		std::unique_lock<std::mutex> lock(_lock);
+
+		// read goes fully through in this item
+		if (offset + size <= _capacity) {
+			// wait for missing bytes to be written if missing
+			while (offset + size > _size)
+				_cond.wait(lock);
+			std::memcpy(buffer, _memory + offset, size);
+			offset += size;
+			if (offset == _capacity) {
+				offset = 0;
+				if (!_next)
+					_push();
+				return _next;
+			}
+			else {
+				return shared_from_this();
+			}
+		}
+
+		// offset too big, musst read in next item
+		else if (offset >= _capacity) {
+			// wait for a next item to be added if mssing
+			while (!_next)
+				_cond.wait(lock);
+			// read in next item
+			offset -= _capacity;
+			lock.unlock();
+			return _next->read(buffer, size, offset);
+		}
+
+		// partial read in this item and the next
+		else {
+			std::size_t partial_size = _capacity - offset;
+			// wait for missing bytes to be written if missing
+			while (_capacity > _size)
+				_cond.wait(lock);
+			std::memcpy(buffer, _memory + offset, partial_size);
+			size -= partial_size;
+			offset = 0;
+			buffer += partial_size;
+			// wait for a next item to be added if mssing
+			while (!_next)
+				_cond.wait(lock);
+			lock.unlock();
+			return _next->read(buffer, size, offset);
+		}
+	}
+
+	std::shared_ptr<block_buffer> write(const char* buffer, std::size_t size, std::size_t& offset) {
+		std::unique_lock<std::mutex> lock(_lock);
+
+		// write goes fully through in this item
+		if (offset + size <= _capacity) {
+			std::memcpy(_memory + offset, buffer, size);
+			_size = std::max(offset + size, _size);
+			offset += size;
+			_cond.notify_all();
+			return shared_from_this();
+		}
+
+		// offset too big, musst write in next item
+		else if (offset >= _capacity) {
+			// add a new item if the is no next one
+			if (!_next)
+				_push();
+			offset -= _capacity;
+			lock.unlock();
+			std::shared_ptr<block_buffer> head = _next->write(buffer, size, offset);
+			_cond.notify_all();
+			return head;
+		}
+
+		// partial write in this item and the next
+		else {
+			std::size_t partial_size = _capacity - offset;
+			std::memcpy(_memory + offset, buffer, partial_size);
+			_size = _capacity;
+			// add a new item if the is no next one
+			if (!_next)
+				_push();
+			lock.unlock();
+			size -= partial_size;
+			offset = 0;
+			buffer += partial_size;
+
+			_cond.notify_all();
+			return _next->write(buffer, size, offset);
+		}
+	}
+private:
+	char* _memory;
+	std::size_t _capacity;
+	std::size_t _size;
+	std::shared_ptr<block_buffer> _next;
+	std::mutex _lock;
+	std::condition_variable _cond;
+
+	void _push() {
+		if (!_next) {
+			_next = std::make_shared<block_buffer>(_capacity);
+		}
+		else {
+			std::shared_ptr<block_buffer>* head = &_next;
+			while (*head)
+				head = &(*head)->_next;
+			*head = std::make_shared<block_buffer>(_capacity);
+		}
+	}
+};
+
+class sync_streambuf : public std::basic_streambuf<char, std::char_traits<char>> {
+public:
+	sync_streambuf(const std::shared_ptr<block_buffer>& block)
+		: _block(block)
+		, _offset(0)
+	{ }
+
+	sync_streambuf(std::shared_ptr<block_buffer>&& block)
+		: _block(std::move(block))
+		, _offset(0)
+	{ }
+
+protected:
+	int_type underflow() override final
+	{
+		char current = 0;
+		std::size_t offset = _offset;
+		_block->read(&current, 1, offset).reset();
+		return traits_type::to_int_type(current);
+	}
+
+	int_type uflow() override final
+	{
+		char current = 0;
+		_block = _block->read(&current, 1, _offset);
+		return traits_type::to_int_type(current);
+	}
+
+	int_type pbackfail(int_type ch) override final
+	{
+		if (_offset == 0) {
+			return traits_type::eof();
+		}
+		else if (ch != traits_type::eof()) {
+			_offset--;
+			return ch;
+		}
+		else {
+			char current = 0;
+			std::size_t offset = --_offset;
+			_block = _block->read(&current, 1, offset);
+			return traits_type::to_int_type(current);
+		}
+	}
+
+	std::streamsize showmanyc() override final
+	{
+		return _block->size() - _offset;
+	}
+
+	sync_streambuf::int_type overflow(int_type ch) override final
+	{
+		char current = traits_type::to_char_type(ch);
+		_block = _block->write(&current, 1, _offset);
+		return traits_type::to_int_type(current);
+	}
+
+	std::streamsize xsgetn(char* s, std::streamsize n) override final
+	{
+		_block = _block->read(s, static_cast<std::size_t>(n), _offset);
+		return n;
+	}
+
+	std::streamsize xsputn(const char* s, std::streamsize n) override final
+	{
+		_block = _block->write(s, static_cast<std::size_t>(n), _offset);
+		return n;
+	}
+private:
+	std::shared_ptr<block_buffer> _block;
+	size_t _offset;
+};
 
 int main()
 {
